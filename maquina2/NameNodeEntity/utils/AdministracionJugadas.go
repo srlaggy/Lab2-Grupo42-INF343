c504f19ce7eae5512ed2d53d1544aa9e91d856c6
Nemeki <chenniy@gmail.com>
2021-10-31 10:29:50 -0300

    feat(NameNode): :construction: Avance en las funciones de NameNode
    Requiero lectura, y meditar algunas decisiones antes de continuar

--- maquina2/NameNodeEntity/utils/AdministracionJugadas.go
+++ maquina2/NameNodeEntity/utils/AdministracionJugadas.go
@@ -47,26 +47,61 @@ func iniciarRegistroJugadas(){
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 func elegirDataNode(jugador string, ronda string) string {
-	Direcciones = ["1", "2", "3"]
-	//TO-DO: Reemplazar por las direcciones ip de los pcs de la u
+	Direcciones := [3]int{"1", "2", "3"}
+	//TO-DO: Reemplazar por las direcciones ip de los pcs de la u,
+	// o las direcciones de prueba segun sea el caso
+	
 	var seleccionada int= rand.Intn(3)
 	ip = Direcciones[seleccionada]
-	var lineContent string =  jugador + " " + ronda + " " + ip
+	var lineContent string =  jugador + " " + ronda + " " + ip + "\n"
 	var fileName string = "jugadas.txt"
 	file, error1 := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	_, error2 := file.WriteString(lineContent)
 	return ip
 }
 
-func iniciarDataNode(jugador string, ronda string, ip string){
-	//Enviar estos datos al datanode con el ip dado
+func iniciarDataNode(jugador string, ronda string){
+	ip = elegirDataNode(jugador string, ronda string)
+	//TO-DO: Revisar si se hace así
 }
+
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Función: entregarJugada
 // -> Localiza la ubicación de una combinación de jugador y
-// ronda y envia la jugada a ese dataNode, en caso de no existir
-// dicha combinación llama a "elegirDataNode"
+// ronda y retorna la direccion ip del dataNode correspondiente,
+// en caso de no existir dicha combinación llama a "elegirDataNode"
+// Recibe: jugador string, ronda string y jugada como string
+// Retorna: jugador string, ronda string y jugada como string
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+func encontrarDataNode(jugador string, ronda string) string ip{
+	var ip string
+	file, error1 := os.OpenFile("jugadas.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	scanner := bufio.NewScanner(f)
+	scanner := bufio.NewScanner(file)
+	flag = false
+    for scanner.Scan(){
+		ubicacion = scanner.Text()
+		s := strings.Fields(ubicacion)
+		if(jugador == s[0]){
+			if(ronda == s[1]){
+				ip = s[2]
+				flag = true
+			}
+		}
+
+    }
+	if(!flag){
+		ip = elegirDataNode(jugador, ronda)
+	}
+	return ip
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Función: entregarJugada
+// -> Con la ayuda de encontrarDataNode obtiene la direccion
+// ip para el jugador en la ronda dada y le envia la jugada al
+// dataNode correspondiente para su registro.
 // Recibe: jugador string, ronda string y jugada como string
 // Retorna: jugador string, ronda string y jugada como string
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -75,6 +110,9 @@ func entregarJugada(jugador string, ronda string, jugada string)(jugador string,
 	var lineContent string =  jugador + " " + ronda
 	var fileName string = "jugadas.txt"
 	file, error1 := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	ip = encontrarDataNode(jugador, ronda)
+
+	return jugador, ronda, jugada
 
 }
 
@@ -87,7 +125,9 @@ func entregarJugada(jugador string, ronda string, jugada string)(jugador string,
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-func solicitarJugadasRonda(jugador string, ronda string) {}
+func solicitarJugadasRondas(jugador string, ronda string) int []{
+		ip = encontrarDataNode(jugador, i)
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Función: devolverJugadasRondas
@@ -97,5 +137,11 @@ func solicitarJugadasRonda(jugador string, ronda string) {}
 // Recibe: jugador string
 // Retorna: todas las jugadas de un jugador
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-func devolverJugadasRondas(jugador string) []int{}
+func devolverJugadasRondas(jugador string) string{
+	
+	for i < 3{
+		i++
+		solicitarJugadasRondas(jugador, i)
+	}
+}
 
