4bb3a739bda2e7e9d413733c6e7a9480a8c43112
Nemeki <chenniy@gmail.com>
2021-11-02 18:56:37 -0300

    :construction: Viendo esto de los servidores
    Auqnue puede ser que sin el resultado de los proto me estanque un poco

---
+++ maquina1/DataNodeEntity/src/DataNodeRegistro/playerRecordDN/playerRecordDN.go
@@ -0,0 +1,41 @@
+package playerRecordDN
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	rp "lab/datanode/proto/playerRecordDN"
+	dr "lab/datanode/src/DataNodeRegistro"
+	ut "lab/datanode/utils"
+)
+
+const (
+	address = "localhost"
+	protocolo_grpc = "tcp"
+	port_grpc = "60002" //Cambiar puerto
+)
+
+// ----- FUNCIÓN: Devolver registro del jugador ----- // --> DataNode actua como servidor
+type server struct {
+	rp.UnimplementedPlayerRecordServiceServer
+}
+
+// funcion: conecta con el service PlayerRecord
+func (s *server) PlayerRecord(ctx context.Context, in *rp.RecordReq) (*rp.RecordResp, error) {
+	log.Printf("Received %v", in.Player) // 
+	return &rp.RecordResp{Records: dr.DevolverJugadas(in.player, in.game)}, nil
+}
+
+// funciones: crea la conexión
+func Grpc_func() {
+	lis, err := net.Listen(protocolo_grpc, ":"+port_grpc)
+	ut.FailOnError(err, "Failed to listen")
+
+	s := grpc.NewServer()
+	rp.RegisterPlayerRecordServiceServer(s, &server{})
+	log.Printf("Servidor grpc escuchando en el puerto %v", port_grpc)
+	ut.FailOnError(s.Serve(lis), "Failed to serve")
+}
